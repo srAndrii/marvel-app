3abed2ee8c8f8569773fde26c45dca78eccc58f4
Andrii <55akif5548@gmail.com>
2022-03-31 22:25:09 +0300

    Практика работы с GIT

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -22,7 +22,7 @@ const App = () => {
         <div className="app">
             <AppHeader/>
             <main>
-                {/* <ErrorBoundary>
+                <ErrorBoundary>
                     <RandomChar/>
                 </ErrorBoundary>
                 <div className="char__content">
@@ -33,9 +33,9 @@ const App = () => {
                         <CharInfo charId={selectedChar} />
                     </ErrorBoundary>
                 </div>
-                <img className="bg-decoration" src={decoration} alt="vision" /> */}
-            <AppBanner />
-            <ComicsList/>
+                <img className="bg-decoration" src={decoration} alt="vision" />
+            {/* <AppBanner />
+            <ComicsList/> */}
                     
             </main>
         </div>
